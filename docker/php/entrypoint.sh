c201bc2fbf3fe379a6c8129824dade030b8d882d
Strannik <strannik.mas@gmail.com>
2026-06-11 18:08:29 +0700

    fix volumes in docker-compose.yml fix entrypoint.sh - only for log directory

--- docker/php/entrypoint.sh
+++ docker/php/entrypoint.sh
@@ -1,4 +1,5 @@
-#!/bin/sh
-mkdir -p /var/www/html/temp /var/www/html/log
-chown -R www-data:www-data /var/www/html/temp /var/www/html/log
+sh#!/bin/sh
+set -e
+mkdir -p /var/www/html/log
+chown -R www-data:www-data /var/www/html/log
 exec docker-php-entrypoint apache2-foreground
